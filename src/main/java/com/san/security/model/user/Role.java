e02268e793c454314c61bd8e2eb1ff68ce33df10
sandersonferraz <sandersonferraz@gmail.com>
2024-03-16 14:36:31 -0300

    Implement UserDetails in User model, and create Role model

---
+++ src/main/java/com/san/security/model/user/Role.java
@@ -0,0 +1,24 @@
+package com.san.security.model.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_role")
+public class Role {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private  Boolean active;
+}
--- src/main/java/com/san/security/model/user/User.java
+++ src/main/java/com/san/security/model/user/User.java
@@ -1,11 +1,13 @@
 package com.san.security.model.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -14,7 +16,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Entity
 @Table(name = "_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
@@ -23,4 +25,45 @@ public class User {
     private String lastName;
     private String email;
     private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Role role;
+
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.getName()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
